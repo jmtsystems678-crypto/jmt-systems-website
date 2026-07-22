fda4f268dbd718b4a874112f2d83b7f2f76256b8
jmtsystems678-crypto <jmtsystems678@gmail.com>
2026-07-22 17:30:44 +0000

    Update app-config.js

--- app-config.js
+++ app-config.js
@@ -1,5 +1,6 @@
 window.JMT_CONFIG = {
   supabaseUrl: 'https://lkonilmpbvunxzehzpbr.supabase.co',
   supabasePublishableKey: 'sb_publishable_WAxKyHR2jAjjaJQxDQfEqA_PAJvVzSJ',
+  siteUrl: 'https://jmt-systems-one.vercel.app',
   adminEmail: 'ababiofrederick3639@gmail.com'
 };
